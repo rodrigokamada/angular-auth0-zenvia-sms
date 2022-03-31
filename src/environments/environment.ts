eba0cc057ffb7a0565277c0a9faf1be992e3546d
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-03-31 16:24:27 -0300

    Adding the Auth0 settings.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  auth0: {
+    domain: 'dev-5tf99p7c.us.auth0.com',
+    clientId: 'GBPB42qhMWCtvrwGmYxvm5cbHXU68nzG',
+    redirectUri: 'http://localhost:4200/profile',
+  },
 };
 
 /*
