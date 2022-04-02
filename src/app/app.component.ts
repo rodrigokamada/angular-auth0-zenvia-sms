4505c73a5a0a51ffd93deec2d5f1dd6f253873cf
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-04-02 15:50:07 -0300

    Adding the menu.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-auth0-zenvia-sms';
+export class AppComponent implements OnInit {
+
+  isAuthenticated: boolean;
+
+  constructor(@Inject(DOCUMENT) private document: Document,
+              private authService: AuthService) {
+    this.isAuthenticated = false;
+  }
+
+  public ngOnInit(): void {
+    this.authService.isAuthenticated$.subscribe((success: boolean) => {
+      this.isAuthenticated = success;
+    });
+  }
+
+  public signOut(): void {
+    this.authService.logout({
+      returnTo: this.document.location.origin,
+    });
+  }
+
 }
